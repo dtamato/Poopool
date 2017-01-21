d7e501429ae4d82064115b22de64e824a6dee1c6
Dave Tamayo <djbtamayo>
2017-01-20 22:52:45 -0500

    - Created player splash that pushes objects in the pool

---
+++ Assets/Scripts/PlayerSplash.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class PlayerSplash : MonoBehaviour {
+
+    [SerializeField] float splashMoveSpeed = 0.1f;
+    [SerializeField] float lifetime = 1;
+
+    void Start ()
+    {
+        Destroy(this.gameObject, lifetime);
+        this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 50);
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.CompareTag("FillModifier"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
