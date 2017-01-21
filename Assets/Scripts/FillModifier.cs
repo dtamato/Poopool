717098debec6f9cb559b42f2068aa9fd48e4ea92
Dave Tamayo <djbtamayo>
2017-01-20 21:49:21 -0500

    - Created team areas that detect when a poop or toy enters it - Poop adds to the fill bar when it's in an area - Toy reduces the fill bar when it's in an area

---
+++ Assets/Scripts/FillModifier.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class FillModifier : MonoBehaviour {
+
+    [SerializeField] float fillAddition = 0.0001f;
+
+    public float GetFillRate()
+    {
+        return fillAddition;
+    }
+}
---
+++ Assets/Scripts/TeamArea.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider2D))]
+public class TeamArea : MonoBehaviour {
+
+	[SerializeField] Image fillBar;
+
+    float fillRate = 0;
+
+    void Start ()
+    {
+        if(fillBar)
+        {
+            fillBar.fillAmount = 0;
+        }
+    }
+
+    void Update ()
+    {
+        if (fillBar.fillAmount < 1)
+        {
+            fillBar.fillAmount += fillRate;
+
+            if(fillBar.fillAmount == 1)
+            {
+                Debug.Log("Game ended!");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("FillModifier"))
+        {
+            fillRate += other.GetComponent<FillModifier>().GetFillRate();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("FillModifier"))
+        {
+            fillRate -= other.GetComponent<FillModifier>().GetFillRate();
+        }
+    }
+}
